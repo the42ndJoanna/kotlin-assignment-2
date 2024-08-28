92995f6f3c2bac7813cacdc09fd0bf0146c77430
Zhihan Huang <joanna.huang.zhihan@gmail.com>
2024-08-28 14:30:47 +0800

    feat: print all products with adjusted prices

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,18 @@
+import kotlinx.coroutines.runBlocking
+import model.InventoriesService
+import model.ProductsService
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val productsService = ProductsService()
+    val inventoryService = InventoriesService()
+    val productManager = ProductManager(productsService, inventoryService)
+
+    runBlocking {
+        val products = productManager.getProductsWithAdjustedPrices()
+
+        products.forEach { product ->
+            println("$product")
+        }
+    }
+
 }
